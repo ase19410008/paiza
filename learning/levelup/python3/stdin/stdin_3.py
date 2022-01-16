184acfc01c0daea8a168ed134b912fcc158b03d7
ase19410008 <ase19410008@wiz.ac.jp>
2022-01-16 20:00:11 +0900

    add stdin_3

---
+++ learning/levelup/python3/stdin/stdin_3.py
@@ -0,0 +1,3 @@
+# URL https://paiza.jp/works/mondai/stdin/stdin_3
+for i in input().split():
+    print(i)
---
+++ learning/levelup/python3/stdin/stdin_n_line.py
@@ -0,0 +1,3 @@
+# URL https://paiza.jp/works/mondai/stdin/stdin_n_line
+for i in range(int(input)):
+    print(input())
