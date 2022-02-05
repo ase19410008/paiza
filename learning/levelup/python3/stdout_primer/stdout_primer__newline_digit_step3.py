582107ce0928dc04a82b2520403e708a55583506
ase19410008 <ase19410008@wiz.ac.jp>
2022-02-05 22:36:48 +0900

    solve spnds4

---
+++ learning/levelup/python3/stdout_primer/stdout_primer__newline_digit_step3.py
@@ -0,0 +1,2 @@
+# URL https://paiza.jp/works/mondai/stdout_primer/stdout_primer__newline_digit_step3
+[print(i) for i in [8, 1, 3]]
---
+++ learning/levelup/python3/stdout_primer/stdout_primer__newline_digit_step4.py
@@ -0,0 +1,2 @@
+# URL https://paiza.jp/works/mondai/stdout_primer/stdout_primer__newline_digit_step4
+[print(i) for i in [813, 1, 2, 923874, 23648, 782356, 3256, 2342, 24324, 112]]
