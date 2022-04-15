f1733ca7cba4a8aa3a28fc0071c39428f0766288
ase19410008 <ase19410008@wiz.ac.jp>
2022-04-15 20:21:52 +0900

    solve boss.py

--- learning/levelup/python3/stdout_primer/variable_array/boss.py
+++ learning/levelup/python3/stdout_primer/variable_array/boss.py
@@ -1,1 +1,16 @@
 # URL https://paiza.jp/works/mondai/stdout_primer/stdout_primer__variable_array_boss
+N, M = [int(i) for i in input().split()]
+A = [int(i) for i in input().split()]
+B = [int(i) for i in input().split()]
+
+AB = []
+stt, stp = 0, 0
+for i in B:
+    stp += i
+    AB.append(A[stt:stp])
+    stt += i
+    
+for i in range(M):
+    for j in range(B[i] - 1):
+        print(AB[i][j], end=' ')
+    print(AB[i][B[i] - 1])
